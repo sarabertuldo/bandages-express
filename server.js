4204f9d10c012bb24cb9fcbc5e357a1454b5c7f0
Sara Bertuldo <sarabertuldo@gmail.com>
2021-04-20 20:31:02 -0500

    preps for deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const session = require("express-session");
 const userRoutes = require("./routes/user.routes");
 const gearRoutes = require("./routes/gear.routes");
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 const bodyParser = require("body-parser");
 app.use(passport.initialize());
@@ -29,9 +29,9 @@ app.use("/users", userRoutes);
 app.use("/gear", gearRoutes);
 
 app.get("/", (req, res) => res.send("Bandages App"));
-// app.get("*", (req, res) => res.redirect("/"));
-app.get("*", (req, res) => {
-  res.sendFile("/build/index.html", { root: __dirname + "/" });
-});
+app.get("*", (req, res) => res.redirect("/"));
+// app.get("*", (req, res) => {
+//   res.sendFile("/build/index.html", { root: __dirname + "/" });
+// });
 
 app.listen(port, () => console.log(`The club is open on port ${port}!`));
